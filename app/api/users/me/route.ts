76b54a6858cacf78c05dd1d4320fbfb9f871fcaf
Nadeesh <spnadeeshnimsara@gmail.com>
2025-05-04 16:34:06 +0530

    Create api to get current user details

---
+++ app/api/users/me/route.ts
@@ -0,0 +1,38 @@
+import prisma from "@/lib/prisma";
+import { decrypt } from "@/lib/session";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  try {
+    const cookie = (await cookies()).get("session")?.value;
+
+    if (!cookie) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const session = await decrypt(cookie);
+    const userId = session?.userId;
+
+    if (!userId) {
+      return NextResponse.json({ error: "Invalid session" }, { status: 401 });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId.toString() },
+      select: {
+        name: true,
+        email: true,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(user);
+  } catch (error: any) {
+    console.error("Error fetching user:", error.message);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
